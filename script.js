d56bf8fa82497a5e1e1a02cc0a5b188df62d4b71
EduPanage <edupanage.dev@gmail.com>
2026-09-01 10:08:06 -0400

    Style: Changing the root var for 'nav-links'

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 $(document).ready(() => {
+  const savedTheme = localStorage.getItem("theme");
 
-    const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    $("body").addClass("dark-theme");
+    $("#darktheme").prop("checked", true);
 
-    if (savedTheme === "dark") {
-        $('body').addClass('dark-theme');
-        $('#darktheme').prop('checked', true);
+  } else {
+    $("body").removeClass("dark-theme");
+    $("#darktheme").prop("checked", false);
+  }
+
+  $("#darktheme").change(function () {
+    if ($(this).is(":checked")) {
+      $("body").addClass("dark-theme");
+      localStorage.setItem("theme", "dark");
+    } else {
+      $("body").removeClass("dark-theme");
+      localStorage.setItem("theme", "light");
     }
-
-    $('#darktheme').change(function() {
-
-        if ($(this).is(':checked')) {
-            $('body').addClass('dark-theme');
-            localStorage.setItem("theme", "dark");
-        } else {
-            $('body').removeClass('dark-theme');
-            localStorage.setItem("theme", "light");
-        }
-
-    });
-
+  });
 });
