d2f0fe6e71361ee772f2560d2751977e188650b6
EduPanage <edupanage.dev@gmail.com>
2026-09-01 09:19:38 -0400

    Feat: Implementing the Dark-theme (Without the LocalStorage)

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ $(document).ready(() => {
     $('#darktheme').change(function() {
         // Verifica se o checkbox do switch está marcado
         if ($(this).is(':checked')) {
-            $('body').addClass('dark-mode');
+            $('body').addClass('dark-theme');
         } else {
-            $('body').removeClass('dark-mode');
+            $('body').removeClass('dark-theme');
         }
     });
 });
+
